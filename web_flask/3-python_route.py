b4a7853636b4aa1d3608af8acdabf05e317b4885
Austin <Martinaustin319@gmail.com>
2024-01-28 03:10:27 -0800

    web framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -20,7 +20,7 @@ def c_text(text):
 
 @app.route("/python", strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
+def python_text(text="is cool"):
     """Displays 'C' followed by the value of <text>."""
     return 'Python {}'.format(text.replace('_', ' '))
 
