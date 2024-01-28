158cd1e71ff40cbd886f64713bd816b3beeeb756
Austin <Martinaustin319@gmail.com>
2024-01-28 11:54:31 -0800

    web framework

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,7 +15,7 @@ def cities():
     return render_template('8-cities_by_states.html', states=states)
 
 @app.teardown_appcontext
-def closedown(exc):
+def closedown(exe):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
