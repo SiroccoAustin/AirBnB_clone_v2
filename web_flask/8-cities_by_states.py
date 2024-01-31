cc6fd1f89da8451336b5ef96f7e82c2001a096fd
Austin <Martinaustin319@gmail.com>
2024-01-30 23:53:22 -0800

    web framework

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,13 +10,14 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_states():
     """Displays an HTML page with a list of all states and related cities."""
-    states = storage.all(State)
+    states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 @app.teardown_appcontext
 def closedown(exc):
     """Remove the current SQLAlchemy session."""
-    return storage.close()
+    storage.close()
 
 if __name__ == '__main__':
+    storage.reload()
     app.run(host='0.0.0.0', port=5000)
